40053b9300c24ff6846840b0472de4041303c286
ktx3 <ktx3@protonmail.com>
2022-06-19 21:15:28 +0000

    Create OCI container entrypoint script

---
+++ README.md
@@ -0,0 +1,28 @@
+# OCI container entrypoint
+
+This is free and unencumbered software released into the public domain.
+
+The `oci-entrypoint.sh` script provides a mechanism for dropping root
+privileges within a container and ensuring that an unprivileged user exists
+with the desired UID/GID.
+
+## Usage
+
+In a Dockerfile, install the script, and set it as the container's entrypoint:
+
+    COPY ["oci-entrypoint.sh", "/usr/local/bin/"]
+    ENTRYPOINT ["/bin/sh", "/usr/local/bin/oci-entrypoint.sh"]
+    CMD ["/bin/sh"]
+
+## Configuration
+
+The entrypoint reads configuration from `/etc/oci-entrypoint-config.sh` if it
+exists, or the following environment variables can be defined:
+
+- `DISABLE_SETPRIV`: do not use `setpriv` for dropping root privileges
+- `USER_NAME`: unprivileged user name, or `root` to disable (default: `user`)
+- `USER_UID`: unprivileged user UID (default: `1000`)
+- `USER_GID`: unprivileged user GID (default: same as UID)
+- `USER_HOME`: unprivileged user home directory (default: `/home/$USER_NAME`)
+- `USER_PATH_PRE`: additional directories to add to the beginning of PATH
+- `USER_PATH_POST`: additional directories to add to the end of PATH
---
+++ oci-entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+# This is free and unencumbered software released into the public domain.
+
+# OCI container entrypoint
+
+set -e -u
+
+# Default config
+: "${USER_NAME:=user}"
+: "${USER_UID:=1000}"
+: "${USER_GID:=${USER_UID:?}}"
+: "${USER_HOME:=/home/${USER_NAME:?}}"
+
+# Default environment
+: "${LANG:=C.UTF-8}"
+: "${LC_COLLATE:=C}"
+: "${TZ:=UTC}"
+
+# Custom config
+! test -e /etc/oci-entrypoint-config.sh || . /etc/oci-entrypoint-config.sh
+
+# Set up the unprivileged user
+if test root = "${USER_NAME:?}"; then
+    USER_HOME=/root
+elif id -u -- "${USER_NAME:?}" >/dev/null 2>&1; then
+    # Update UID/GID of existing user
+    USER_GID_OLD="$(id -g -- "${USER_NAME:?}")"
+    if test "x${USER_GID_OLD:?}" != "x${USER_GID:?}"; then
+        groupmod -g "${USER_GID:?}" -- "$(id -g -n -- "${USER_NAME:?}")"
+        find / \
+            -not \( -path /dev -prune \) \
+            -not \( -path /proc -prune \) \
+            -not \( -path /run -prune \) \
+            -not \( -path /sys -prune \) \
+            -gid "${USER_GID_OLD:?}" \
+            -exec chgrp "${USER_GID:?}" '{}' +
+    fi
+
+    USER_UID_OLD="$(id -u -- "${USER_NAME:?}")"
+    if test "x${USER_UID_OLD:?}" != "x${USER_UID:?}"; then
+        usermod -u "${USER_UID:?}" -- "${USER_NAME:?}"
+        find / \
+            -not \( -path /dev -prune \) \
+            -not \( -path /proc -prune \) \
+            -not \( -path /run -prune \) \
+            -not \( -path /sys -prune \) \
+            -uid "${USER_UID_OLD:?}" \
+            -exec chown "${USER_UID:?}" '{}' +
+    fi
+else
+    # Create new user (suppress warnings about existing home directory e.g.
+    # when mounting volumes into it)
+    groupadd -g "${USER_GID:?}" -- "${USER_NAME:?}"
+    useradd -m -g "${USER_GID:?}" -u "${USER_UID:?}" -- "${USER_NAME:?}" 2>/dev/null
+fi
+
+# Environment variables
+HOME="${USER_HOME:?}"
+USER="${USER_NAME:?}"
+PATH="${HOME:?}/.local/bin:${PATH:?}"
+test -z "${USER_PATH_PRE:-}" || PATH="${USER_PATH_PRE:?}:${PATH:?}"
+test -z "${USER_PATH_POST:-}" || PATH="${PATH:?}:${USER_PATH_POST:?}"
+export HOME LANG LC_COLLATE TZ USER
+
+# Drop privileges
+if test root = "${USER_NAME:?}"; then
+    exec "${@}"
+elif command -v setpriv >/dev/null && test -z "${DISABLE_SETPRIV:-}"; then
+    setpriv \
+        --clear-groups \
+        --bounding-set=-all \
+        --inh-caps=-all \
+        "--regid=${USER_GID:?}" \
+        "--reuid=${USER_UID:?}" \
+        -- "${@}"
+else
+    printf 'oci-entrypoint.sh: warning: setpriv not available or disabled\n' >&2
+    if test 0 -eq "${#}"; then
+        runuser -s "${SHELL:-/bin/sh}" -- "${USER_NAME:?}"
+    else
+        runuser -u "${USER_NAME:?}" -- "${@}"
+    fi
+fi
